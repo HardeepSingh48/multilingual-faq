db12e9be96b5979803306414a0f62a454f571487
HardeepSingh48 <137910457+HardeepSingh48@users.noreply.github.com>
2025-02-02 18:57:01 +0530

    Added PostgreSQL dependency

--- faq_project/settings.py
+++ faq_project/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -81,10 +83,7 @@ WSGI_APPLICATION = 'faq_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
